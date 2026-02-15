552e062b90270ca72415c87413985239b4ec2bbe
donkvalorant <donkvalorant@gmail.com>
2026-02-15 19:10:13 +0300

    fix socket transport (polling+ws)

--- src/lib/socket.ts
+++ src/lib/socket.ts
@@ -1,6 +1,10 @@
 import { io } from "socket.io-client";
 
 export const socket = io("https://gamblevs-production.up.railway.app", {
-  transports: ["websocket"],
   path: "/socket.io",
+  transports: ["polling", "websocket"], // ✅ önce polling, sonra websocket
+  upgrade: true,
+  rememberUpgrade: true,
+  withCredentials: true,
+  timeout: 20000,
 });
